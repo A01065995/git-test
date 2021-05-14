c47c531656bb8745fcdc60b2a7d9dca75698e975
gustavo <a01065995@itesm.mx>
2021-05-13 20:30:17 -0400

    Initial Commit

---
+++ git test/git_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_testApp.swift
+//  git test
+//
+//  Created by user185762 on 5/13/21.
+//
+
+import SwiftUI
+
+@main
+struct git_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
